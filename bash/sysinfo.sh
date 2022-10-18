3c30a9270282ed4c1a21bb4cda70615c2644e8ee
tulsi <tulsibodara@gmail.com>
2022-10-18 19:48:18 -0400

    updated sysinfo script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-#Printing the host name
-echo "Hostname: "`hostname`
+#Printing the host name and assigning it to a variable
+HOSTNAME=`hostname`
 
-#displaying all host information
-echo "Hostinformation: "
-hostnamectl
+#displaying all host information and assigning into variable
+OSNAME=`uname`
+OSVERSION=`uname -r`
 
-#the host IP address
-echo "host IPaddress: "
-hostname -I
+#the host IP address and assigning variable into it
+HOSTIPADDRESS=`hostname -i`
 
-#presenting the space available in root directory which is easy to read
-echo "Space available in root directory: "
-df -H /
+#presenting the space available in root directory which is easy to read and assigning variable into it
+ROOTSPACE=`df -h | grep "/dev/sda3" | awk '{print $2}'`
+
+cat<<EOF 
+
+Report for $HOSTNAME
+=======================
+
+FQDN: $HOSTNAME
+Operating System name and version: $OSNAME/$OSVERSION
+IP ADDRESS: $HOSTIPADDRESS
+Root Filesystem space: $ROOTSPACE
+
+=======================
+
+EOF
+
+
 
